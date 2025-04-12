ab08154e587d61e59d0e7e8df657ad84c71148b1
gemechuH <gemechuhunduma100@gmail.com>
2025-04-12 13:17:16 -0800

    i create two folder and install tailwind css for my task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App = () => {
-  return <div>NILE TECHNOLOGY SOLUTIONS</div>;
+  return (
+    <div>
+      NILE TECHNOLOGY SOLUTIONS
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App
